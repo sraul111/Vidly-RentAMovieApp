44d09c1895a3cb381ca46e6d5bf8f9af878feda4
Sujit Raul <sraul111@gmail.com>
2020-04-28 21:18:04 +0530

    enable easy loading of MembershipType for reference of Customer entity

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,23 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customer
         public ActionResult Index()
         {
-            return View();
+            var customers = _context.Customers.Include(c => c.MemberShipType).ToList();
+            return View(customers);
         }
 
+        [Route("customer/details/{id}")]
         public ActionResult Details(int id)
         {
+            var customer = _context.Customers.ToList().SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                return HttpNotFound();
 
-            return View();
+            return View(customer);
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{Id=1,IsSubscribedToNewsletter = true,Name = "sujit"},
+                new Customer{Id=2,IsSubscribedToNewsletter = true,Name = "Prabhu"},
+                new Customer{Id=3,IsSubscribedToNewsletter = true,Name = "Bunu"}
+            };
+
         }
     }
 }
