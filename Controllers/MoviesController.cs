03936dbce501069797321eaf1185bbd1f2ca4eea
Sujit Raul <sraul111@gmail.com>
2020-04-28 16:33:05 +0530

    implementing movies controller

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -9,7 +9,13 @@ using Vidly.ViewModels;
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
-    {
+    { 
+        List<Movie> movies = new List<Movie>
+            {
+                new Movie {Id =1, Name = "Shrek !"},
+                new Movie {Id = 2, Name = "Die Hard"}
+            };
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -42,18 +48,28 @@ namespace Vidly.Controllers
             return Content("id =" + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            
+            var movieCollectionViewModel = new MoviesCollectionViewModel()
+            {
+                Movies = movies
+            };
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format($"pageIndex={pageIndex}&sortBy={sortBy}"));
+            return View(movieCollectionViewModel);
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        [Route("movies/detail/{id}")]
+        public ActionResult Detail(int id)
+        {
+            var movie = movies.SingleOrDefault(x => x.Id == id);
+            if (movie == null)
+                //return HttpNotFound();
+                return Content("the movie is not in our db.");
+            return View(movie);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year,int month)
         {
             return Content(year + "/" +month);
---
+++ ViewModels/MoviesCollectionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class MoviesCollectionViewModel
+    {
+        public List<Movie> Movies { get; set; }
+    }
+}
