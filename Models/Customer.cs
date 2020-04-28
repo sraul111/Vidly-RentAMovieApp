b92daedf3c0c4bddc135ac7f533ede33c59da26e
Sujit Raul <sraul111@gmail.com>
2020-04-28 17:06:06 +0530

    add memershiptype domain and-migration for it

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,5 +9,9 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MemberShipType MemberShipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
+
     }
 }
